0c1f37165c1bdf2ce35e94a8c3597a548bc62841
bjkillen <blaine.j.killen@gmail.com>
2024-09-22 13:52:55 -0700

    simpleified logic

--- app/src/models/WargearClassifier.tsx
+++ app/src/models/WargearClassifier.tsx
@@ -7,27 +7,30 @@ class WargearClassifier {
     static Classify(wargear: Wargear) {
         const wargearAbilities = new WargearAbilities(wargear);
 
+        const wargearAttacksVariableNumericalValue = VariableNumericalValueParser.Parse(wargear.attacks);
         const wargearDamageVariableNumericalValue = VariableNumericalValueParser.Parse(wargear.damage);
 
         let wargearClassifications = [];
 
-        if ((wargear.strength >= 9 || (wargearAbilities.anti != null && wargearAbilities.anti[0] === 'VEHICLE')) &&
+        if ((wargear.strength >= 9 || wargearAbilities.anti?.[0] === 'VEHICLE') &&
             (wargearDamageVariableNumericalValue.numericalVal >= VariableNumericalValueParser.Parse("D6").numericalVal ||
                 wargearAbilities.melta != null)
         ) {
             wargearClassifications.push(WargearClassification.AntiVehicle);
-        } else if (wargear.strength >= 6 && 
-            wargearDamageVariableNumericalValue.numericalVal >= 3 && Math.abs(wargear.armorPenetration) >= 2
+        }
+
+        if (wargear.strength >= 6 && 
+            wargearDamageVariableNumericalValue.numericalVal >= 3 &&
+            Math.abs(wargear.armorPenetration) >= 2 &&
+            wargearAttacksVariableNumericalValue.numericalVal > 2
         ) {
             wargearClassifications.push(WargearClassification.AntiHeavyInfantry);
         }
 
-        const wargearAttacksVariableNumericalValue = VariableNumericalValueParser.Parse(wargear.attacks);
-
         if ((wargear.strength <= 5 && ((wargearAbilities.sustainedHits?.numericalVal ?? 0) >= 2 ||
                 wargearAbilities.blast ||
                 wargearAttacksVariableNumericalValue.numericalVal >= 4 ||
-                (wargearAbilities.anti != null && wargearAbilities.anti[0] === 'INFANTRY'))) ||
+                wargearAbilities.anti?.[0] === 'INFANTRY')) ||
                 (wargear.strength >= 6 && wargearAttacksVariableNumericalValue.numericalVal >= 10)
             ) {
                 wargearClassifications.push(WargearClassification.AntiInfantry);
